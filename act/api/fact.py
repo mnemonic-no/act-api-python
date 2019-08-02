ba87653ff164a22e74c8ad73c15b86932c8bef1d
Fredrik Borg <fredrik.borg@gmail.com>
2019-08-02 15:14:59 +0200

    Support flags returned from backend (#23)
    * minor spelling correction
    
    * remove > from documentation
    
    * make act object pickable
    
    * bump version
    
    * support flag field in new backed

--- act/api/fact.py
+++ act/api/fact.py
@@ -299,6 +299,7 @@ class Fact(ActBase):
         Field("value", default=""),
         Field("id", serializer=False),
         # For now, do not serialize/deserialize source
+        Field("flags", serializer=False),
         Field("source", deserializer=False, serializer=False),
         Field("timestamp", serializer=False),
         Field("last_seen_timestamp", serializer=False),
