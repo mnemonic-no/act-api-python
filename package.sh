4de603098d644bd6b60fa94f39b0f097c045dbcd
Fredrik Borg <fredrik.borg@gmail.com>
2019-12-09 09:38:14 +0100

    remove packaging scripts from repo (#42)

--- package.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-tar cvzf python-act.tgz -C .. python-act/LICENSE python-act/AUTHORS python-act/README.md python-act/setup.py python-act/act/base.py python-act/act/helpers.py python-act/act/obj.py python-act/act/utils.py python-act/act/fact.py  python-act/act/__init__.py python-act/act/schema.py
-echo "Created archive file ./python-act.tgz"
