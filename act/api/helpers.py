6f05efbeb8663c7f98d913dcbf69f1cd0cd95ed9
Fredrik Borg <fredrikb@mnemonic.no>
2020-04-07 15:38:34 +0200

    try/except around whole block

--- act/api/helpers.py
+++ act/api/helpers.py
@@ -512,17 +512,16 @@ Return: List of facts
 """
     facts = []
 
-    my_uri = urllib.parse.urlparse(uri)
-
-    scheme = my_uri.scheme
-    path = my_uri.path
-    query = my_uri.query
-    addr = my_uri.hostname
-
     try:
+        my_uri = urllib.parse.urlparse(uri)
+
+        scheme = my_uri.scheme
+        path = my_uri.path
+        query = my_uri.query
+        addr = my_uri.hostname
         port = my_uri.port
     except ValueError as e:
-        raise act.api.base.ValidationError(f"Error parsing port in URI {uri}: {e}")
+        raise act.api.base.ValidationError(f"Error parsing URI: {uri}: {e}")
 
     if not (scheme and addr):
         raise act.api.base.ValidationError("URI requires both scheme and address part")
